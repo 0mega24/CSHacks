99bafbb981ce05101252fc9839509a0c75157b00
0mega24 <omegamech24@gmail.com>
2023-09-23 16:13:08 -0400

    alpha check and set creator

--- image_parsing.py
+++ image_parsing.py
@@ -17,6 +17,11 @@ def getImageArray(image_file):
 
     return image_pixel_array
 
+def getImageDim(image_file):
+    image = Image.open(image_file)
+    width, height = image.size
+    return width, height
+
 def getHSVAfromRGBA(pixel_array, x, y):
     """
     A method to take an x and y coordinate and a pixel array to return the HSVA values
@@ -25,6 +30,15 @@ def getHSVAfromRGBA(pixel_array, x, y):
 
     return list(colorsys.rgb_to_hsv(R/255, G/255, B/255)) + [A]
 
+def nonVectorizedAlphaCheckerAndSetCreator(pixel_array, x, y, unique_value_set):
+    """
+    Takes a list containing HSVA and checks to see if the alpha value is not 0 and then adds it to a set if it is not equal to 0
+    """
+    HSVA = getHSVAfromRGBA(pixel_array, x, y)
+    if int(HSVA[3]) != 0:
+        unique_value_set.add(HSVA)
+        return unique_value_set
+
 
 def main():
     """
@@ -32,11 +46,12 @@ def main():
     """
     image_file_path = f"S:/Windows 10 Host OS/Minecraft/Resources/Textures/item/amethyst_shard.png"
     
-    pixels = getImageArray(image_file_path)
-    H, S, V, A = getHSVAfromRGBA(pixels, 8, 8)
-    H *= 360
-    print(round(H, -1), round(S, 2), round(V, 2), A)
-    print(pixels[8, 8])
+    
+    # pixels = getImageArray(image_file_path)
+    # H, S, V, A = getHSVAfromRGBA(pixels, 8, 8)
+    # H *= 360
+    # print(round(H, -1), round(S, 2), round(V, 2), A)
+    # print(pixels[8, 8])
 
 if __name__ == "__main__":
     main()
