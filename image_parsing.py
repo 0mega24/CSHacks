525cec77aceb2809b14eae099aae3dfc95f094e1
0mega24 <omegamech24@gmail.com>
2023-09-24 03:22:06 -0400

    HSVA to RGBA function and refinement of documentation

--- image_parsing.py
+++ image_parsing.py
@@ -2,48 +2,61 @@
 The backbone of the project a module to take an image input and parse it down to a
 list/set of values of each pixel containing either the hue, saturation, value, and alpha value or the RGBA components.
 Ignoring alpha 0 pixels while still preserving all other alpha values
+
+The second major portion is the 4 layers of sorting and clustering
 Sean Lidy
 """
 
 from PIL import Image
 import colorsys
 import numpy
-import time
 
 def openImage(image_file):
     """
-    Open the image file to be used in other functions to avoid having multiple instances of it in memory
+    - A method to open the image file to be used in other functions to avoid having multiple instances of it in memory
+    - Format the method to match the naming convention that I have been using
     """
     return Image.open(image_file)
 
 def getImageArray(image):
     """
-    A method that takes a image file and returns the array of all RGBA values with an x and y coordinate system
+    - A method to load the image into an array that can be iterated over
+    - Format the method to match the naming convention that I have been using
     """
-    image_pixel_array = image.load()
-
-    return image_pixel_array
+    return image.load()
 
 def getImageDim(image):
     """
-    Take an open pillow image and return the width and height of the open file
+    - Take an open pillow image and return the width and height of the open file
+    - Wrap the size function to return 2 values and not a list
     """
     width, height = image.size
     return width, height
 
-def getHSVAfromRGBA(RGBA):
+def getHSVAfromRGBA(pixel):
     """
-    A method to take an x and y coordinate and a pixel array to return the HSVA values
+    - A method to take a pixel object that contains the R, G, B, A values and convert it into the HSVA color space for sorting reasons
+    - Return a tuple containing the H, S, V values as 2 decimal point floats and an int representing the alpha value
     """
-    R, G, B, A = RGBA
-
+    R, G, B, A = pixel
     H, S, V, A = list(colorsys.rgb_to_hsv(R/255, G/255, B/255)) + [A]
 
     return (round(H, 2), round(S, 2), round(V, 2), A)
 
+def getRGBAfromHSVA(pixel):
+    """
+    - A method to take a pixel object that contains the H, S, V, A values and convert it into the RGBA color space for image generation reasons
+    - Return a tuple containing the R, G, B, A values as ints
+    """
+    H, S, V, A = pixel
+    R, G, B, A = list(colorsys.hsv_to_rgb(H, S, V)) + [A]
+
+    return (int(R * 255), int(G * 255), int(B * 255), A)
+
 def checkAlphaAndAddToList(HSVA, value_list):
     """
-    Takes a list containing HSVA and checks to see if the alpha value is not 0 and then adds it to a set if it is not equal to 0
+    - Takes a list containing HSVA and checks to see if the alpha value is not 0 and then adds it to a list if it is not equal to 0
+    - Ignores wether or not it is a unique color or not
     """
     if int(HSVA[3]) != 0:
         value_list.append(HSVA)
@@ -51,6 +64,9 @@ def checkAlphaAndAddToList(HSVA, value_list):
         return value_list
 
 def processImage(image, color_list):
+    """
+    - Takes a pillow image and a color list object and returns a list of all the unique colors within the image
+    """
     pixels = getImageArray(image)
     width, height = getImageDim(image)
     for x in range(width):
