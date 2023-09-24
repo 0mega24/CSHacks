a54a3f039e05a687e73785e1be8f2af263625db1
0mega24 <omegamech24@gmail.com>
2023-09-24 06:17:03 -0400

    clump by hue and value

--- palletFromImage.py
+++ palletFromImage.py
@@ -7,9 +7,11 @@ The second major portion is the 4 layers of sorting and clustering
 Sean Lidy
 """
 
+from itertools import groupby
+import multiprocessing
 from PIL import Image
 import colorsys
-import numpy
+import time
 
 def openImage(image_file):
     """
@@ -61,7 +63,7 @@ def checkAlphaAndAddToList(HSVA, value_list):
     if int(HSVA[3]) != 0:
         value_list.append(HSVA)
 
-        return value_list
+    return value_list
 
 def processImage(image, color_list):
     """
@@ -73,20 +75,83 @@ def processImage(image, color_list):
         for y in range(height):
             HSVA = getHSVAfromRGBA(pixels[x, y])
             checkAlphaAndAddToList(HSVA, color_list)
+
     return list(set(color_list))
+
+def clumpByKey(input_list, key_index):
+    """
+    A more universal method that clumps a list based on a specific key
+    """
+    input_list.sort(key=lambda x: x[key_index])
+    clumped_data = []
+    current_group = []    
+    for item in input_list:
+        if not current_group or item[key_index] == current_group[0][key_index]:
+            current_group.append(item)
+        else:
+            if len(current_group) > 1:
+                clumped_data.append(current_group.copy())
+            else:
+                clumped_data.append(current_group[0])
+            current_group = [item]
+    if current_group:
+        if len(current_group) > 1:
+            clumped_data.append(current_group.copy())
+        else:
+            clumped_data.append(current_group[0])
+
+    return clumped_data
+
+def clumpByHue(unique_color_list):
+    """
+    A helper function to clump and sort by hue
+    """
+
+    return clumpByKey(unique_color_list, 0)
+
+def clumpByValue(unique_color_list):
+    temp_list = []
+    for sublist in unique_color_list:
+        if len(sublist) == 1 or type(sublist) == tuple:
+            temp_list.append(sublist)
+        elif len(sublist) == 2:
+            temp = sublist
+            temp.sort(key=lambda x: x[2])
+            temp_list.append(temp)
+        else:
+            temp = clumpByKey(sublist, 2)
+            temp_list.append(temp)
+    unique_color_list = temp_list
+
+    return temp_list
+
+
 
 def main():
     """
     Main
     """
     image_file_path = f"S:/Windows 10 Host OS/Minecraft/Resources/Textures/item/amethyst_shard.png"
+    # image_file_path = f"S:/Shared Downloads Folder/test.png"
 
     open_image = openImage(image_file_path)
 
     color_list = []
-    color_list = processImage(open_image, color_list)
+    color_list = [(0.76, 0.34, 0.95, 255), (0.73, 0.48, 0.8, 255), (0.16, 0.16, 1.0, 255), (0.73, 0.42, 0.95, 255), (0.91, 0.2, 1.0, 255), (0.72, 0.59, 0.54, 255), (0.72, 0.54, 0.67, 255), (0.72, 0.53, 0.67, 255), (0.72, 0.54, 0.67, 254), (0.72, 0.52, 0.67, 255)]
+    
+    # color_list = processImage(open_image, color_list)
 
     print(color_list)
+
+    color_list = clumpByHue(color_list)
+
+    print(color_list)
+
+    color_list = clumpByValue(color_list)
+
+    print(color_list)
+
+
 
 if __name__ == "__main__":
     main()
